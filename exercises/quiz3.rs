2d4be7b20c62605270d95197498b7d4fb35842fc
Eric Time <windwhinny@gmail.com>
2023-06-22 08:52:15 +0800

    feat: update exercises

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,10 +16,29 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+use core::fmt;
+
+pub enum Grade {
+    Num(f32),
+    Alpha(String)
+}
+
+impl fmt::Display for Grade {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Grade::Alpha(grade) =>  {
+                write!(f, "{}", grade)
+            }
+            Grade::Num(num) => {
+                write!(f, "{}", num)
+            }
+        }
+    }    
+}
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Grade,
     pub student_name: String,
     pub student_age: u8,
 }
@@ -38,7 +57,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Num(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -52,7 +71,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Alpha("A+".to_string()),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
